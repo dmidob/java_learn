8429dbc076f81c0f4f26100a429372de04666787
Dmytro_Dobrovolskyi <dmig40@gmail.com>
2019-11-18 15:52:17 +0200

    Package renamed to camelCase Package with tests added to VCS

---
+++ src/test/java/firstSimpleJavaTasks/BubbleSortTests.java
@@ -0,0 +1,25 @@
+package firstSimpleJavaTasks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class BubbleSortTests {
+
+    private int[] newIntArray = {9,-5,32,-62,192,-225,9};
+    private int[] newIntArraySorted = {-225,-62,-5,9,9,32,192};
+    private int[] newIntSingleElementArray = {0};
+    private int[] newIntSingleElementArraySorted = {0};
+
+
+    @Test
+    public void testSimpleSorting() {
+        assertArrayEquals(newIntArraySorted, BubbleSort.arrayBubbleSortingIntArray(newIntArray));
+    }
+
+    @Test
+    public void testSingleElementArraySorting() {
+        assertArrayEquals(newIntSingleElementArraySorted, BubbleSort.arrayBubbleSortingIntArray(newIntSingleElementArray));
+    }
+
+}
