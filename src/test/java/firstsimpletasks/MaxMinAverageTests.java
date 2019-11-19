68b9cd34a5119114dd6c51748743581fc212e037
Dmytro_Dobrovolskyi <dmig40@gmail.com>
2019-11-19 16:23:10 +0200

    Variables for test arrays (MaxMinAverageTests class) renamed

--- src/test/java/firstsimpletasks/MaxMinAverageTests.java
+++ src/test/java/firstsimpletasks/MaxMinAverageTests.java
@@ -5,31 +5,30 @@ import org.junit.Test;
 
 public class MaxMinAverageTests {
 
-    private int[] newArray = {0,1,2,3,4,5,6,7,8,9};
-    private int[] zeroLengthArray = {};
+    private int[] arrayFrom0To9 = {0,1,2,3,4,5,6,7,8,9};
+    private int[] arrayZeroLength = {};
     private int[] arrayWithNegativeValues = {-5,225,-1,0,25,-61};
 
 
     @Test
     public void testGettingMaxValue() {
-        int n = MaxMinAverage.getMaxIntArrayValue(newArray);
+        int n = MaxMinAverage.getMaxIntArrayValue(arrayFrom0To9);
         Assert.assertEquals(9, n);
     }
 
     @Test
     public void testGettingMaxValueArrayWithNegativeValues() {
-        int n = MaxMinAverage.getMaxIntArrayValue(arrayWithNegativeValues);
-        Assert.assertEquals(225, n);
+        Assert.assertEquals(225, MaxMinAverage.getMaxIntArrayValue(arrayWithNegativeValues));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testGettingMaxValueZeroLengthArray() {
-        MaxMinAverage.getMaxIntArrayValue(zeroLengthArray);
+        MaxMinAverage.getMaxIntArrayValue(arrayZeroLength);
     }
 
     @Test
     public void testGettingMinValue() {
-        int n = MaxMinAverage.getMinIntArrayValue(newArray);
+        int n = MaxMinAverage.getMinIntArrayValue(arrayFrom0To9);
         Assert.assertEquals(0, n);
     }
 
@@ -41,7 +40,7 @@ public class MaxMinAverageTests {
 
     @Test
     public void testGettingAverageValue() {
-        int n = MaxMinAverage.getAverageIntArrayValue(newArray);
+        int n = MaxMinAverage.getAverageIntArrayValue(arrayFrom0To9);
         Assert.assertEquals(4, n);
     }
 
