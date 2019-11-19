dfbde969a0f138255cb1559672b801d6ec8604ba
Dmytro_Dobrovolskyi <dmig40@gmail.com>
2019-11-19 17:12:01 +0200

    Variables renamed for PrimeNumbersTests by "actual" and "expected" postfixes added. Equals assertion is using 2 List-typed arrays now.

--- src/test/java/firstsimpletasks/PrimeNumbersTests.java
+++ src/test/java/firstsimpletasks/PrimeNumbersTests.java
@@ -2,20 +2,18 @@ package firstsimpletasks;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class PrimeNumbersTests {
 
-    private int[] primeNumbersFrom2To100IntArray = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-
-
-
-
+    private final List<Integer> primeNumbersFrom2To100ArrayExpected = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
 
     @Test
     public void testSimpleSorting() {
-        int[] primeNumbersFrom2To100ArrayList= ArrayTypesConvertor.arrayListToIntArrayConversion(PrimeNumbers.identifyPrimeNumbersArrayList());
-        Assert.assertArrayEquals(primeNumbersFrom2To100IntArray, primeNumbersFrom2To100ArrayList);
+        List<Integer> primeNumbersFrom2To100ArrayActual = PrimeNumbers.identifyPrimeNumbersArrayList();
+        Assert.assertEquals(primeNumbersFrom2To100ArrayExpected , primeNumbersFrom2To100ArrayActual);
     }
 }
