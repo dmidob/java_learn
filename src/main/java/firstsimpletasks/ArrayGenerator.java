4aae10300d8382d29dff661fe8ac2cad4dc9b7e3
Dmytro_Dobrovolskyi <dmig40@gmail.com>
2019-11-19 16:38:47 +0200

    Constnt added in ArrayGenerator class for right bound limit value

--- src/main/java/firstsimpletasks/ArrayGenerator.java
+++ src/main/java/firstsimpletasks/ArrayGenerator.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 class ArrayGenerator {
 
+    private static final int LIMIT_FOR_RIGHT_ARRAY_BOUND = 300;
+
     //method for array generating and filling it by random int numbers from 0 to 300 using List array.
     static List<Integer> generateRandomArrayList (int arrayLength) {
         List<Integer> newArrayList = new ArrayList<>();
         for (int i = 0; i < arrayLength; i++) {
-            newArrayList.add(i, (int) (Math.random() * 300));
+            newArrayList.add(i, (int) (Math.random() * LIMIT_FOR_RIGHT_ARRAY_BOUND));
         }
         return newArrayList;
     }
@@ -18,7 +20,7 @@ class ArrayGenerator {
     static int[] generateRandomIntArray (int arrayLength) {
         int[] newIntArray = new int[arrayLength];
         for (int i = 0; i < arrayLength; i++) {
-            newIntArray[i] = (int) (Math.random() * 300);
+            newIntArray[i] = (int) (Math.random() * LIMIT_FOR_RIGHT_ARRAY_BOUND);
         }
         return newIntArray;
     }
